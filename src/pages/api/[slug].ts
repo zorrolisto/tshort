0e66da4e23e75d580fc603776dc0029ed0d085db
julio@theowl.solutions <julio@theowl.solutions>
2022-07-10 19:09:36 -0500

    Refactor: api call

--- src/pages/api/[slug].ts
+++ src/pages/api/[slug].ts
@@ -7,22 +7,20 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const {
     query: { slug },
   } = req;
-  console.log("slug");
-  console.log(slug);
+
   if (!slug || typeof slug !== "string") {
-    res.status(404).send(JSON.stringify({ message: "pls use slug as string" }));
+    res.status(404).json({ message: "pls use slug as string" });
     return;
   }
+
   const data = await prisma.shortLink.findFirst({
     where: { slug: { equals: slug } },
   });
+
   if (!data) {
     res.status(404).send(JSON.stringify({ message: "slug dont found" }));
     return;
   }
-  res.status(200).send(
-    JSON.stringify({
-      ...data,
-    })
-  );
+
+  res.status(200).json(data);
 };
